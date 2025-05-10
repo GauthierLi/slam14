8213c066b4a5fa89af224a4da7db0e85dddf8048
gauthierli <lwklxh@163.com>
2025-05-10 17:14:07 +0800

    add use opencv demo

---
+++ src/BasesLibsExamples/pangolin_eigen_cv_head.h
@@ -0,0 +1,21 @@
+#include <Eigen/Core>
+#include <absl/strings/str_format.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <pangolin/pangolin.h>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+#include <chrono>
+using namespace std;
+using namespace Eigen;
+
+
